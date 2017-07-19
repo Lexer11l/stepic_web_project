76b23800480c65359108abd12e593c830483c6c1
SBT-Meshkov-KA <SBT-Meshkov-KA@sberbank.ru>
2017-07-19 17:09:16 +0300

    3rd sask

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
-# Nginx
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-# Gunicorn
-sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
-gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
+sudo rm /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
