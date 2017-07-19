142cf92f0174ea9ec566b8231c1c0d9609e3354d
SBT-Meshkov-KA <SBT-Meshkov-KA@sberbank.ru>
2017-07-19 17:04:33 +0300

    3rd sask

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+sudo pip install --upgrade django; sudo pip install --upgrade gunicorn
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
