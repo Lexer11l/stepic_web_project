43c546e62955f547316baa45b6213987cc9015ad
SBT-Meshkov-KA <SBT-Meshkov-KA@sberbank.ru>
2017-07-19 13:41:33 +0300

    2nd sask

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
